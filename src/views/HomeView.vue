e13a369c27c6cff0a02ec6e4947bb2530d960e21
minkhant21 <kaungminkhant6889@gmail.com>
2023-01-26 14:49:50 +0630

    computed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="home">
-    <h1>Home Page</h1>
-    <p>I am {{name}}.My age is {{age}}</p>
-    <button @click="handleclick">Click</button>
+    <input type="text" v-model="search">
+    <p>Search name - {{search}}</p>
+    <div v-for="name in filteredNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import { ref ,computed } from 'vue';
 
 export default {
   setup()
   {
-    let name = "kaung min khant";
-    let age = 20;
-    let handleclick = ()=>{
-      console.log('u clicked me')
-    }
-    return {name,age,handleclick}
+    let names = ref(["aungaung","mgmg","KyawKyaw","susu"])
+    let search = ref("");
+    let filteredNames = computed(()=>{
+      return names.value.filter((name)=>{
+        return name.includes(search.value)
+      })
+    })
+    return {names,search,filteredNames};
   }
 }
 </script>
