3cdf19055f4a2e27b7c6851a3bc60cc1fe9c2d1f
minkhant21 <kaungminkhant6889@gmail.com>
2023-01-26 16:02:12 +0630

    lifecyclehook

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="home">
-    <input type="text" v-model="search">
-    <p>Search name - {{search}}</p>
-    <div v-for="name in filteredNames" :key="name">
-      {{ name }}
-    </div>
+      <h1>Posting</h1>
+      <div v-if="show">
+        <div v-for="post in posts" :key="post">
+          <SinglePost :post="post"></SinglePost>
+      </div>
+      </div>
+        
+      
+      <button @click="show=!show">show</button>
+      <button @click="posts.pop()">Pop</button>
   </div>
 </template>
 
 <script>
+import SinglePost from './SinglePost'
 // @ is an alias to /src
 
 import { ref ,computed } from 'vue';
 
 export default {
-  setup()
-  {
-    let names = ref(["aungaung","mgmg","KyawKyaw","susu"])
-    let search = ref("");
-    let filteredNames = computed(()=>{
-      return names.value.filter((name)=>{
-        return name.includes(search.value)
-      })
-    })
-    return {names,search,filteredNames};
+  components: { SinglePost },
+  setup(){
+    let posts = ref([
+     { "title" : "post title 1","body":"Loram has built a decades-long partnership with the rail industry offering the most innovative portfolio of equipment crewed by our expertly-trained operators and technicians. Our contracted services capabilities assure our customers of superior maintenance, unsurpassed productivity and maximum rail availability." , "id" : 1},
+     { "title" : "post title 2","body":"body 2" , "id" : 2},
+    ])
+    let show = ref(true)
+    return {posts,show};
   }
 }
 </script>
---
+++ src/views/SinglePost.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>{{post.title}}</h3>
+    <h3>{{cutpost}}</h3>
+    
+</template>
+
+<script>
+import { computed,onMounted ,onUnmounted ,onUpdated} from 'vue'
+export default {
+    props:["post"],
+    setup(props)
+    {
+        let cutpost = computed(()=>{
+            return props.post.body.substring(0,20)
+        })
+        onMounted(()=>console.log("Mounted"))
+        onUnmounted(()=>console.log("UmMounted"))
+        onUpdated(()=>console.log("Updated"))
+        return {cutpost};
+    }
+}
+</script>
+
+<style>
+
+</style>
